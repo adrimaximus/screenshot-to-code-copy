8035df51e0f80fa1a1897c24911828659b1d08ce
adrimaximus <adri@7inked.com>
2025-10-25 10:38:20 +0700

    [dyad] Memperbaiki kesalahan impor di main.tsx - wrote 1 file(s) + extra files edited outside of Dyad

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-
-// Optional: kalau nanti kamu mau tambah halaman lain,
-// tinggal import dan tambahkan di <Routes> di bawah.
-import NotFound from "./components/NotFound"; // contoh halaman 404 custom (opsional)
+import App from "./App.tsx";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
+    <Toaster toastOptions={{ className: "dark:bg-zinc-950 dark:text-white" }} />
   </React.StrictMode>
 );
