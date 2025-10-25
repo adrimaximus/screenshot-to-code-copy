7c17cf74397009e5d7042d55ab32a7c784584860
adrimaximus <adri@7inked.com>
2025-10-25 10:30:16 +0700

    [dyad] Menghapus router untuk memperbaiki masalah pemuatan - wrote 2 file(s) + extra files edited outside of Dyad

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </Router>
+    <App />
     <Toaster toastOptions={{ className: "dark:bg-zinc-950 dark:text-white" }} />
   </React.StrictMode>
 );
